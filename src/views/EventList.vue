8c5ef74ebc0287215be96f3a28d8d7514e777d8a
happyt <ian.happytuesdays@gmail.com>
2020-04-13 10:44:41 +0100

    Lesson 5

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Events listing</h1>
+  <div>
+    <h1>Events listing</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 'ian' } }"
+      >Show Event</router-link
+    >
+    |
+  </div>
 </template>
