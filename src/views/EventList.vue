d90ebd0a476311876b128cc0c342882d831432ac
happyt <ian.happytuesdays@gmail.com>
2020-04-13 12:00:46 +0100

    Lesson 6

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <h1>Events listing</h1>
+    <EventCard />
     <router-link :to="{ name: 'event-show', params: { id: 'ian' } }"
       >Show Event</router-link
     >
     |
   </div>
 </template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
