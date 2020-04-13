b9eb190a184e74af41e2863522dff594af37fb2a
happyt <ian.happytuesdays@gmail.com>
2020-04-13 15:35:02 +0100

    Lesson 9 axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
     <h1>Events listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
     <BaseIcon name="trending-up" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:' + error.response)
+      })
   }
 }
 </script>
