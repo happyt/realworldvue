7c7327eef9b0eed94c8debcdb725209a1ba213dd
happyt <ian.happytuesdays@gmail.com>
2020-04-16 13:44:39 +0100

    with problem

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: { id: 'abcdef', name: 'Adam' },
+    categories: ['sport', 'news', 'history']
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event.data)
+      })
+    }
+  },
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
   modules: {}
 })
